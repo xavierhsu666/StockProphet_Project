db43cb7072f4d2cc36cce0e46ffd2bf5d0b59d46
boris83418 <boris83418@gmail.com>
2024-03-16 11:39:13 +0800

    更新form用JS append

--- StockProphet_Project/Program.cs
+++ StockProphet_Project/Program.cs
@@ -21,7 +21,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	//pattern: "{controller=StockModel}/{action=predictindex}/{id?}");
+
 	pattern: "{controller=StockModel}/{action=predictindex}");
 
 app.Run();
