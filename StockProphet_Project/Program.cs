4861546038d0043930013248a0d58950b739c109
yushan <j1129w@gmail.com>
2024-03-21 14:29:15 +0800

    修改傳值的方式+Edit 頁面

--- StockProphet_Project/Program.cs
+++ StockProphet_Project/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<StocksContext>(
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
-	options.IdleTimeout = TimeSpan.FromSeconds(10);
+	options.IdleTimeout = TimeSpan.FromSeconds(1000);
 	options.Cookie.HttpOnly = true;
 	options.Cookie.IsEssential = true;
 });
@@ -30,8 +30,8 @@ app.UseAuthorization();
 app.UseSession();
 app.MapControllerRoute(
 	name: "default",
-pattern: "{controller=Home}/{action=Index}");
-//pattern: "{controller=StockModel}/{action=predictindex}/{id?}");
+//pattern: "{controller=Home}/{action=Index}");
+pattern: "{controller=Home}/{action=Index}/{id?}"); 
 //pattern: "{controller=StockModel}/{action=testBuild}");
 
 app.Run();
